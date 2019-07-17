b161676c0d933d7fc68d3960c2fbb0b2e35ec98b
Nestor Vera <nestorvera@me.com>
2019-07-17 20:16:31 +0200

    Use `tailwindcss-plugin-utils` instead of custom logic

--- index.js
+++ index.js
@@ -1,72 +1,19 @@
-var _ = require('lodash')
-var flatten = require('flat')
-
-
-const FLATTEN_CONFIG = { delimiter: '-', maxDepth: 2 }
-const getName = name => name.split('-default').join('')
-
+const tailwindConfig = require('./tailwind.config.js')
+const { buildConfig, something } = require('@hacknug/tailwindcss-plugin-utils')
 
 module.exports = function () {
-  return function ({
-    addUtilities, addComponents, addBase, addVariant,
-    e, prefix, theme, variants, config,
-  }) {
-    const buildConfig = (themeKey, ...fallbackKeys) => {
-      return buildConfigFromTheme(themeKey, ...fallbackKeys) || buildConfigFromArray(themeKey)
-    }
-    const buildConfigFromTheme = (themeKey, ...fallbackKeys) => {
-      const buildObject = ([ modifier, value ]) => [ modifier, { [themeKey]: value } ]
-      const getThemeSettings = (themeKey, fallbackKeys) => {
-        const [newThemeKey, ...newFallbackKeys] = fallbackKeys || []
-        return theme(themeKey, false) || (fallbackKeys.length && getThemeSettings(newThemeKey, [...newFallbackKeys]))
-      }
-
-      const themeSettings = getThemeSettings(themeKey, fallbackKeys)
-      const themeObject = _.isArray(themeSettings) ? _.zipObject(themeSettings, themeSettings) : themeSettings
-      const themeEntries = themeSettings && Object
-        .entries(flatten(themeObject, FLATTEN_CONFIG))
-        .map(entry => buildObject(entry))
-
-      return themeSettings ? _.fromPairs(themeEntries) : false
-    }
-    const buildConfigFromArray = (property) => {
-      const defaultSettings = defaultValues[property]
-      const defaultEntries = defaultSettings && defaultSettings
-        .map((value) => ([value, { [property]: value }]))
-
-      return defaultSettings ? _.fromPairs(defaultEntries) : false
-    }
-
-    const defaultValues = {
-      columnCount: [1, 2, 3],
-      columnRuleStyle: ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset'],
-      columnFill: ['auto', 'balance', 'balance-all'],
-      columnSpan: ['none', 'all'],
-    }
-
+  return function (coreUtils) {
     const pluginUtilities = {
-      'col-count': buildConfig('columnCount'),
-      'col-gap': buildConfig('columnGap', 'gap', 'gridGap'),
-      'col-w': buildConfig('columnWidth'),
-      'col-rule-color': buildConfig('columnRuleColor', 'borderColor'),
-      'col-rule-width': buildConfig('columnRuleWidth', 'borderWidth'),
-      'col-rule-style': buildConfig('columnRuleStyle'),
-      'col-fill': buildConfig('columnFill'),
-      'col-span': buildConfig('columnSpan'),
+      'col-count': buildConfig(coreUtils, tailwindConfig, 'columnCount'),
+      'col-gap': buildConfig(coreUtils, tailwindConfig, 'columnGap', 'gap', 'gridGap'),
+      'col-w': buildConfig(coreUtils, tailwindConfig, 'columnWidth'),
+      'col-rule-color': buildConfig(coreUtils, tailwindConfig, 'columnRuleColor', 'borderColor'),
+      'col-rule-width': buildConfig(coreUtils, tailwindConfig, 'columnRuleWidth', 'borderWidth'),
+      'col-rule-style': buildConfig(coreUtils, tailwindConfig, 'columnRuleStyle'),
+      'col-fill': buildConfig(coreUtils, tailwindConfig, 'columnFill'),
+      'col-span': buildConfig(coreUtils, tailwindConfig, 'columnSpan'),
     }
 
-    Object.entries(pluginUtilities)
-      .filter(([ modifier, values ]) => !_.isEmpty(values))
-      .forEach(([ modifier, values ]) => {
-        const className = _.kebabCase(modifier).split('-').slice(0, 2).join('-')
-        const variantName = Object.keys(Object.entries(values)[0][1])[0]
-        const escapedValues = _.fromPairs(Object.entries(values).map(([modifier, value]) => [e(modifier), value]))
-        const utilities = flatten({ [`.${e(`${className}`)}`]: escapedValues }, FLATTEN_CONFIG)
-
-        addUtilities(
-          _.mapKeys(utilities, (value, key) => getName(key)),
-          variants(variantName, ['responsive'])
-        )
-      })
+    return something(pluginUtilities, coreUtils)
   }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    columnCount: [1, 2, 3],
+    columnRuleStyle: ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset'],
+    columnFill: ['auto', 'balance', 'balance-all'],
+    columnSpan: ['none', 'all'],
+  },
+}
